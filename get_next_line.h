334e2908d68ba7fcc93710637a3cf393d82aab90
Jacksiesta <56738437+Jacksiesta@users.noreply.github.com>
2019-11-08 15:52:29 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 11:38:15 by jherrald          #+#    #+#             */
+/*   Updated: 2019/11/08 14:38:17 by jherrald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 32
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+
+
+int		get_next_line(int fd, char **line);
+
+#endif
