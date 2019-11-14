ee478fd18d128d2c9791b3a18bd6af0a9b0c3d9d
Jacksiesta <56738437+Jacksiesta@users.noreply.github.com>
2019-11-14 18:08:42 +0100

    Add files via upload

---
+++ old_get_next_line.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/10 18:18:33 by jherrald          #+#    #+#             */
+/*   Updated: 2019/11/14 14:07:00 by jherrald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+
+
+int		len_line(char *s, char **line)
+{
+	size_t		len;
+	size_t		size_whole_line;
+	char		*temp;
+
+	size_whole_line = 0;
+	while(*s++)
+		size_whole_line++;
+	len = 0;
+	while(s[len] != '\0' || *s[len] != '\n')
+		len++;
+	if (s[len] == '\n')
+	{
+		*line = ft_substr(s, 0, len);
+		*temp = ft_strdup((char const)*s);
+		free(s);
+		*s = temp;
+		if (s[len] == '\0')
+			ft_strdel(*s);
+	}
+	else
+	{
+		*line = ft_substr(*s, 0, len);
+		ft_strdel(*s);
+	}
+	return (1);
+}
+
+int		result(char **s, char **line, int res, int fd)
+{
+	if (res < 0)
+		return (-1);
+	if (res == 0) //reading completed
+		return (0);
+	return (len_line(&s, line)); //got to ft who returns 1 + save the line read at current address of static var stored
+}
+
+int		get_next_line(int fd, char **line)
+{
+	char			buff[BUFFER_SIZE + 1];
+	static char		x[fd];
+	char			*temp;
+	int				res;
+
+	if (fd < 0 || !line)
+		return (-1);
+	while (res = read(fd, buff, BUFFER_SIZE))
+	{
+		buff[res] = '\0';
+		if (x[fd] == NULL) //check if pointer is empty
+			x[fd] = ft_strdup(buff); // malloc 
+		else
+			temp = ft_strjoin(x[fd], buff);
+			free(x[fd]);
+			x[fd] = temp;
+		if (ft_strchr(x[fd], '\n'))
+			break;
+	}
+	if (res < 0)
+		return (-1);
+	if (res == 0)
+		return (0);
+	return (result(*x, *line, res, fd));
+}
+		
---
+++ old_get_next_line_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 11:21:36 by jherrald          #+#    #+#             */
+/*   Updated: 2019/11/13 21:03:14 by jherrald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int		x;
+	char				*new;
+	size_t				lenght;
+
+	if (!s)
+		return (NULL);
+	lenght = 0;
+	while (s[lenght] != '\0')
+		lenght++;
+	if ((lenght - (size_t)start) < len)
+		len = lenght - (size_t)start;
+	if ((size_t)start > lenght;)
+		len = 0;
+	if (!(new = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	x = 0;
+	while (len--)
+	{
+		new[x] = s[start + x];
+		x++
+	}
+	new[x] = '\0';
+	return (new);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		x;
+	char 	*new;
+	
+	x = 0;
+	while (s[x])
+		x++;
+	if (!(new = (char *)malloc(sizeof(char) * (x + 1))))
+		return (NULL);
+	x = 0;
+	while (s[x] != '\0')
+	{
+		new[x] = s[x];
+		x++;
+	}
+	new[x] = '\0';
+	return (new);
+}
+
+void	ft_strdel(char **c)
+{
+	if (c)
+	{
+		free(*c)
+		*c = NULL;
+	}
+	return ;
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		x;
+
+	x = 0;
+	while (s[x])
+	{
+		if (s[x] == ((char)c)
+			return ((char *)&s[x]);
+		x++;
+	}
+	if (s[x] == (char)c)
+		return ((char *)&s[x]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		x;
+	int		y;
+	char	*join;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	x = 0;
+	y = 0;
+	while (s1[x])
+		x++;
+	while (s2[y])
+		y++;
+	if (!(join = (char *)malloc(sizeof(char) * (x + y + 1))))
+		return (NULL);
+	y = 0;
+	while (s1[y++])
+		join[y] = s1[y];
+	x = 0;
+	while (s2[x++])
+		join[y + x] = s2[x];
+	join[y + x] = '\0';
+	return (join);
+}
